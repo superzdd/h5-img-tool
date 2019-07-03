dfcc1cd45126f0552a8493ba5bb073ce2076e1c3
superzdd <superzdd@sina.com>
2019-07-03 16:57:21 +0800

    添加README，规范文件名

--- get-pixels-test.js
+++
@@ -1,9 +0,0 @@
-var getPixels = require('get-pixels');
-
-getPixels('page15_button_01.png', function(err, pixels) {
-    if (err) {
-        console.log('Bad image path');
-        return;
-    }
-    console.log('got pixels', pixels.shape.slice());
-});
